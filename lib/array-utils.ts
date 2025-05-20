f26822e855e168219b7c4e4b7aaa58dcccd71515
Micolen <89450869+guava3s@users.noreply.github.com>
2025-05-20 16:23:01 +0800

    [dev] 调整数组工具类

--- lib/array-utils.ts
+++ lib/array-utils.ts
@@ -1,4 +1,4 @@
-function getIntersection<T>(arr1: T[], arr2: T[]): T[] {
+export function getIntersection<T>(arr1: T[], arr2: T[]): T[] {
     const set = new Set<T>(arr2);
     return [...new Set<T>(arr1)].filter(x => set.has(x));
 }
